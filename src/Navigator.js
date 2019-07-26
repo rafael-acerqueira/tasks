4f9c707357d6b22f0b3a69188cc4399e728d0845
Rafael <rafael.acerqueirah@gmail.com>
2019-07-26 15:08:34 -0300

    Add DrawerNavigator

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,6 +1,58 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+import React from 'react'
+import {
+	createSwitchNavigator,
+	createDrawerNavigator,
+	createAppContainer
+} from 'react-navigation'
 import Schedule from '../src/screens/Schedule'
 import Auth from '../src/screens/Auth'
+import global from './styles/global'
+
+const MenuRoutes = createDrawerNavigator(
+	{
+		Today: {
+			name: 'Today',
+			screen: props => <Schedule title="Hoje" daysAhead={0} {...props} />,
+			navigationOptions: {
+				title: 'Hoje'
+			}
+		},
+		Tomorrow: {
+			name: 'Tomorrow',
+			screen: props => <Schedule title="Amanhã" daysAhead={1} {...props} />,
+			navigationOptions: {
+				title: 'Amanhã'
+			}
+		},
+		Week: {
+			name: 'Week',
+			screen: props => <Schedule title="Semana" daysAhead={7} {...props} />,
+			navigationOptions: {
+				title: 'Semana'
+			}
+		},
+		Month: {
+			name: 'Month',
+			screen: props => <Schedule title="Mês" daysAhead={30} {...props} />,
+			navigationOptions: {
+				title: 'Mês'
+			}
+		}
+	},
+	{
+		initialRouteName: 'Today',
+		contentOptions: {
+			labelStyle: {
+				fontFamily: global.fontFamily,
+				fontWeight: 'normal',
+				fontSize: 20
+			},
+			activeLabelStyle: {
+				color: '#080'
+			}
+		}
+	}
+)
 
 const MainRoutes = createSwitchNavigator(
 	{
@@ -10,7 +62,7 @@ const MainRoutes = createSwitchNavigator(
 		},
 		Home: {
 			name: 'Home',
-			screen: Schedule
+			screen: MenuRoutes
 		}
 	},
 	{ initialRouteName: 'Auth' }
