cdc28a42c270d021ab3512fc93f5a0bd82846295
Rafael <rafael.acerqueirah@gmail.com>
2019-07-26 09:38:00 -0300

    Create service to API

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { Alert } from 'react-native'
+
+const api = axios.create({
+	baseURL: 'http://192.168.122.1:3000'
+})
+
+export const setToken = token => {
+	axios.defaults.headers.common['Authorization'] = `bearer ${token}`
+}
+
+export const showError = err => {
+	Alert.alert('Ops! Ocorreu um problema', `Mensagem: ${err}`)
+}
+
+export default api
